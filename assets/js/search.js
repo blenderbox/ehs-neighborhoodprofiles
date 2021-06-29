c87fa956c9fe6e13821357601818feb2ff239e3e
Chris Doss <chrisdoss@Chris-MacBook-Pro.local>
2021-06-29 18:56:02 -0400

    refactor search js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,19 +1,19 @@
-
-$('form[role="search"] .btn-primary').on('click', () => {
+const handleSearch = (e) => {
   const search = $('form[role="search"] input').val()
+  e.preventDefault();
   if (search.length > 2) {
     window.location.href = `/ehs-neighborhoodprofiles/search_results/index.html?search=${search}`;
   }
+}
+
+$('form[role="search"] .btn-primary').on('click', (e) => {
+  handleSearch(e);
 })
 
 $("#global-search").on('show.bs.collapse', function () {
   $(this).keypress(function(e){
     if (e.keyCode == 13) {
-      const search = $('form[role="search"] input').val()
-      e.preventDefault();
-      if (search.length > 2) {
-        window.location.href = `/ehs-neighborhoodprofiles/search_results/index.html?search=${search}`;
-      }
+      handleSearch(e);
       return false;
     }
   })
